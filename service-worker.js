deb186daf79cd31f581ee284dff248e0f2aeff93
charlesnadar <charles.nadar@gmail.com>
2026-05-14 18:50:23 +0530

    menu debugging

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Bump CACHE_VERSION when you push significant updates.
    Installed users will get the fresh version on next open. */
 
-var CACHE_VERSION = 'pixslay-v17';
+var CACHE_VERSION = 'pixslay-v18';
 
 var APP_SHELL = [
   '/',
