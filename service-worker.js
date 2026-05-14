2a92d5b5be70e1ce73c595578740c777339cd935
charlesnadar <charles.nadar@gmail.com>
2026-05-14 17:32:31 +0530

    menu

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Bump CACHE_VERSION when you push significant updates.
    Installed users will get the fresh version on next open. */
 
-var CACHE_VERSION = 'pixslay-v10';
+var CACHE_VERSION = 'pixslay-v11';
 
 var APP_SHELL = [
   '/',
